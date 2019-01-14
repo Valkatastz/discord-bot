3ce972e459e38a3ce7164107eb6f861b039d963a
Valentin Valev <valentins613@gmail.com>
2019-01-14 18:16:30 +0000

    help function added

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ var bot = new Discord.Client({
 });
 bot.on('ready', function (evt) {
     logger.info('Connected');
-    logger.info('Logged in as: ');
+    logger.info('Logged in as: Exerax Official');
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
@@ -29,6 +29,27 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 bot.sendMessage({
                     to: channelID,
                     message: 'Pong!'
+                });
+            break;
+            // Just add any case commands if you want to..
+         }
+     }
+});
+
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'help':
+                bot.sendMessage({
+                    to: author,
+                    message: 'Laws & Server Rules: https://club.exerax.com/community/forum/rules-guidelines/'
                 });
             break;
             // Just add any case commands if you want to..
